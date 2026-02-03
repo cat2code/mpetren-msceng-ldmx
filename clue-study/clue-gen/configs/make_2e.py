02ee2052471d024b5d3fa338a89ccad68d6246e4
Eliot Montesino Petrén <eliot.mp99@gmail.com>
2026-02-03 16:43:17 +0100

    cleaning stuff up: deleting old code

--- clue-study/clue-gen/configs/make_2e.py
+++
@@ -1,2 +0,0 @@
-# python -c "from LDMX.SimCore import generators; print([g for g in dir(generators) if 'e' in g.lower()])"
-
--- first_histogram_analysis/.local/lib/python3.11/site-packages/hepstats/splot/warnings.py
+++
@@ -1,9 +0,0 @@
-"""
-Specific warnings for the `splot` submodule
-"""
-
-from __future__ import annotations
-
-
-class AboveToleranceWarning(UserWarning):
-    pass
